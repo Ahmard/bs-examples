7faac7cb47089b6ffa080a0d0dd5dd1bca5048d6
Ahmard <Ahmard06@gmail.com>
2021-04-22 16:57:09 +0100

    Welcome message added

--- index.php
+++ index.php
@@ -9,10 +9,11 @@
 </head>
 
 <body>
+<h1>Welcome to BS Example</h1>
+<h3>
+    Please join our <a href="https://chat.whatsapp.com/HHgQJMDaOze740fbxWvEvy">WhatsApp Group</a> if you haven't
+</h3>
     <ol>
-        <li>
-            <a href="https://chat.whatsapp.com/HHgQJMDaOze740fbxWvEvy">WhatsApp Group</a>
-        </li>
     </ol>
 </body>
 </html>
