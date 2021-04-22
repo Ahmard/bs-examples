c47cc7b63486f1252cf838255c906e75a351c1d7
Ahmard <Ahmard06@gmail.com>
2021-04-22 16:52:14 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>BS Examples</title>
+</head>
+
+<body>
+    <ol>
+        <li>
+            <a href="https://chat.whatsapp.com/HHgQJMDaOze740fbxWvEvy">WhatsApp Group</a>
+        </li>
+    </ol>
+</body>
+</html>
